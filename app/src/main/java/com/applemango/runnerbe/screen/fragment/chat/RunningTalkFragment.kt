0be38d4c6e535c9a357e2f7f9a130a439ac73487
whwogjs96 <68106399+whwogjs96@users.noreply.github.com>
2022-12-30 18:01:39 +0900

    UI - 러닝톡 화면 추가
    러닝톡 프래그먼트 기초 화면 구성

---
+++ app/src/main/java/com/applemango/runnerbe/screen/fragment/chat/RunningTalkFragment.kt
@@ -0,0 +1,11 @@
+package com.applemango.runnerbe.screen.fragment.chat
+
+import androidx.fragment.app.viewModels
+import com.applemango.runnerbe.R
+import com.applemango.runnerbe.databinding.FragmentRunningTalkBinding
+import com.applemango.runnerbe.screen.fragment.base.BaseFragment
+
+class RunningTalkFragment: BaseFragment<FragmentRunningTalkBinding>(R.layout.fragment_running_talk) {
+
+    private val viewModel : RunningTalkViewModel by viewModels()
+}
