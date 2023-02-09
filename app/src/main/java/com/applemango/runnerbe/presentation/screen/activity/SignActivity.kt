24f2f82e3d07211c6f68bc0bb7696b1243de07a1
Jaeheon Cho <68106399+whwogjs96@users.noreply.github.com>
2023-02-10 00:05:10 +0900

    UI - 이용약관 화면 구현
    추가정보 입력 화면의 이용약관 화면 구현

--- app/src/main/java/com/applemango/runnerbe/presentation/screen/activity/SignActivity.kt
+++ app/src/main/java/com/applemango/runnerbe/presentation/screen/activity/SignActivity.kt
@@ -1,6 +1,7 @@
 package com.applemango.runnerbe.presentation.screen.activity
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.runtime.Composable
@@ -12,6 +13,7 @@ import dagger.hilt.android.AndroidEntryPoint
 class SignActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.e("key hash", com.kakao.util.helper.Utility.getKeyHash(this))
         setContent {
             SignNavHost()
         }
---
+++ app/src/main/java/com/applemango/runnerbe/presentation/screen/fragment/additionalinfo/AgreeToTermsFragment.kt
@@ -0,0 +1,50 @@
+package com.applemango.runnerbe.presentation.screen.fragment.additionalinfo
+
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
+import com.applemango.runnerbe.R
+import com.applemango.runnerbe.databinding.FragmentAgreeToTermsBinding
+import com.applemango.runnerbe.presentation.screen.fragment.additionalinfo.terms.AgreeToTermsViewModel
+import com.applemango.runnerbe.presentation.screen.fragment.base.BaseFragment
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
+
+class AgreeToTermsFragment :BaseFragment<FragmentAgreeToTermsBinding>(R.layout.fragment_agree_to_terms) {
+
+    private val viewModel : AgreeToTermsViewModel by viewModels()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.vm = viewModel
+        binding.fragment = this
+        viewLifecycleOwner.lifecycleScope.launch {
+            viewModel.allCheck.collect {
+                binding.termsAllCheckBox.isChecked = it
+            }
+        }
+        binding.termsAllCheckBox.setOnClickListener {
+            viewModel.serviceTerms.value = binding.termsAllCheckBox.isChecked
+            viewModel.privacyTerms.value = binding.termsAllCheckBox.isChecked
+            viewModel.locationServiceTerms.value = binding.termsAllCheckBox.isChecked
+        }
+    }
+
+    fun moveToServiceUseTerms() {
+        val intent = Intent(Intent.ACTION_VIEW, Uri.parse("https://runnerbe.xyz/policy/service.txt"))
+        startActivity(intent)
+    }
+
+    fun moveToPrivacyTerms() {
+        val intent = Intent(Intent.ACTION_VIEW, Uri.parse("https://runnerbe.xyz/policy/privacy-collect.txt"))
+        startActivity(intent)
+    }
+
+    fun moveToLocationServiceUseTerms() {
+        val intent = Intent(Intent.ACTION_VIEW, Uri.parse("https://runnerbe.xyz/policy/location.txt"))
+        startActivity(intent)
+    }
+}
