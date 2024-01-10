493d2df64ddc4dfe44c03231ed7f8755d3e3710d
whwogjs96 <68106399+whwogjs96@users.noreply.github.com>
2024-01-10 23:02:46 +0900

    UI - 페이스등록 화면 틀 추가

---
+++ app/src/main/java/com/applemango/runnerbe/presentation/screen/fragment/mypage/faceinfo/PaceInfoRegistFragment.kt
@@ -0,0 +1,10 @@
+package com.applemango.runnerbe.presentation.screen.fragment.mypage.faceinfo
+
+import com.applemango.runnerbe.R
+import com.applemango.runnerbe.databinding.FragmentRegistPaceInfoBinding
+import com.applemango.runnerbe.presentation.screen.fragment.base.BaseFragment
+
+class PaceInfoRegistFragment: BaseFragment<FragmentRegistPaceInfoBinding>(R.layout.fragment_regist_pace_info) {
+
+
+}
